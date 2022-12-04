7b4f0d265a64b8ec97574bae1fa9a9f749fa3b49
Teva90 <89648026+Teva90@users.noreply.github.com>
2022-12-04 20:38:18 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "foldable mini drone",
+    productNumber: "85631",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "ruko fu pro drone",
+    productNumber: "810292",
+    paymentStatus: "paid",
+    shipping: "pending",
+  },
+  {
+    productName: "drone with camera",
+    productNumber: "812345",
+    paymentStatus: "paid",
+    shipping: "delivered",
+  },
+  {
+    productName: "Dij air 2s",
+    productNumber: "899211",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "lozenge drone",
+    productNumber: "823131",
+    paymentStatus: "unpaid",
+    shipping: "declined",
+  },
+];
